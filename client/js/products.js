d3ca2812ccbb3870f771e53d7d4c901ea18fb10e
Nicoll <nicoll.escobar01@uceva.edu.co>
2024-09-06 12:05:26 -0500

    subiendo

---
+++ client/js/products.js
@@ -0,0 +1,24 @@
+const productos = [
+    {
+        id: 1,
+        productName: "Resistencias",
+        price: 5000,
+        quanty: 1,
+        img: "./media/resistencia.jpeg"  //AGREGAR RUTA DE LAS IMAGENES
+    },
+    {
+        id: 2,
+        productName: "Capacitores",
+        price: 6000,
+        quanty: 1,
+        img: "./media/resistencia.jpeg"  //AGREGAR RUTA DE LAS IMAGENES
+    },
+    {
+        id: 3,
+        productName: "Cables",
+        price: 1000,
+        quanty: 1,
+        img: "./media/resistencia.jpeg"  //AGREGAR RUTA DE LAS IMAGENES
+    },
+
+];
